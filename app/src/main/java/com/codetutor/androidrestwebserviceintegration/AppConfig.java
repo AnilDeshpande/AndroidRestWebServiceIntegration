5a0e9315dc0195cbb6a4a4b6a16a0ed28eaef8df
Anil Deshpande <anildesh82@gmail.com>
2018-05-29 15:27:07 +0530

    Added some more code

--- app/src/main/java/com/codetutor/androidrestwebserviceintegration/AppConfig.java
+++ app/src/main/java/com/codetutor/androidrestwebserviceintegration/AppConfig.java
@@ -1,9 +1,12 @@
 package com.codetutor.androidrestwebserviceintegration;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.SharedPreferences;
 
 import com.codetutor.androidrestwebserviceintegration.network.Util;
+import com.codetutor.androidrestwebserviceintegration.restbean.Author;
+import com.google.gson.GsonBuilder;
 
 /**
  * Created by anildeshpande on 5/2/18.
@@ -17,6 +20,8 @@ public class AppConfig extends Application {
     public static API_ENDPOINTS selectedEndPoint;
     public boolean isEmulator;
 
+    static Context context;
+
     public static enum API_ENDPOINTS{
         localhost, remote
     }
@@ -29,12 +34,32 @@ public class AppConfig extends Application {
         sharedPreferences = getSharedPreferences("appprefrences.xml",MODE_PRIVATE);
         editor = sharedPreferences.edit();
         isEmulator = Util.isEmulator();
+        context = getApplicationContext();
 
     }
 
     public static void saveUserName(String username){
         editor.putString("username",username);
         editor.commit();
+    }
+
+    public static void saveSuccessfulLoginUser(String jsonString){
+        editor.putString("user",jsonString);
+        editor.commit();
+    }
+
+    public static Author getSavedSuccessfulAuthor(){
+        Author author = new GsonBuilder().create().fromJson(sharedPreferences.getString("user",null), Author.class);
+        return author;
+    }
+
+    public static void saveSessionTokenValue(String token){
+        editor.putString("token",token);
+        editor.commit();
+    }
+
+    public static String getSessionTokenValue(String token){
+        return sharedPreferences.getString("token",null);
     }
 
     public static  String getSavedUserName(){
@@ -67,6 +92,10 @@ public class AppConfig extends Application {
 
     public static boolean getSeverEndPointPreference(){
         return sharedPreferences.getBoolean("endpoint", true);
+    }
+
+    public static Context getContext(){
+        return context;
     }
 
 }
